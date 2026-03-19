17740f41c973ad1ec6fe9905fe56ad19710294de
hjun0907 <59383493+hjun0907@users.noreply.github.com>
2026-03-19 23:15:37 +0800

    Update Program.cs

--- HomeNetSentinel/Program.cs
+++ HomeNetSentinel/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 using System.Diagnostics;
 using System.Globalization;
 using System.Text;
